4dd7d2d17c66c2878a63b8b6ed145bfe275b785a
Alexander <alkexan@yandex.com>
2022-11-17 10:18:37 +0300

    Update types

--- bele.h
+++ bele.h
@@ -7,44 +7,44 @@ extern "C" {
 /*
  * beget.c
  */
-uint8_t be8getv(void*);
-uint16_t be16getv(void*);
-uint32_t be24getv(void*);
-uint32_t be32getv(void*);
-uint64_t be64getv(void*);
+unsigned char be8getv(void*);
+unsigned short be16getv(void*);
+unsigned int be24getv(void*);
+unsigned int  be32getv(void*);
+unsigned long be64getv(void*);
 
-unsigned int be8get(void*, uint8_t*);
-unsigned int be16get(void*, uint16_t*);
-unsigned int be24get(void*, uint32_t*);
-unsigned int be32get(void*, uint32_t*);
-unsigned int be64get(void*, uint64_t*);
+unsigned int be8get(void*, unsigned char*);
+unsigned int be16get(void*, unsigned short*);
+unsigned int be24get(void*, unsigned int*);
+unsigned int be32get(void*, unsigned int*);
+unsigned int be64get(void*, unsigned long*);
 
-unsigned int be8put(void*, uint8_t);
-unsigned int be16put(void*, uint16_t);
-unsigned int be24put(void*, uint32_t);
-unsigned int be32put(void*, uint32_t);
-unsigned int be64put(void*, uint64_t);
+unsigned int be8put(void*, unsigned char);
+unsigned int be16put(void*, unsigned short);
+unsigned int be24put(void*, unsigned int);
+unsigned int be32put(void*, unsigned int);
+unsigned int be64put(void*, unsigned long);
 
 /*
  * leget.c
  */
-uint8_t le8getv(void*);
-uint16_t le16getv(void*);
-uint32_t le24getv(void*);
-uint32_t le32getv(void*);
-uint64_t le64getv(void*);
+unsigned char le8getv(void*);
+unsigned short le16getv(void*);
+unsigned int le24getv(void*);
+unsigned int le32getv(void*);
+unsigned long le64getv(void*);
 
-unsigned int le8get(void*, uint8_t*);
-unsigned int le16get(void*, uint16_t*);
-unsigned int le24get(void*, uint32_t*);
-unsigned int le32get(void*, uint32_t*);
-unsigned int le64get(void*, uint64_t*);
+unsigned int le8get(void*, unsigned char*);
+unsigned int le16get(void*, unsigned short*);
+unsigned int le24get(void*, unsigned int*);
+unsigned int le32get(void*, unsigned int*);
+unsigned int le64get(void*, unsigned long*);
 
-unsigned int le8put(void*, uint8_t);
-unsigned int le16put(void*, uint16_t);
-unsigned int le24put(void*, uint32_t);
-unsigned int le32put(void*, uint32_t);
-unsigned int le64put(void*, uint64_t);
+unsigned int le8put(void*, unsigned char);
+unsigned int le16put(void*, unsigned short);
+unsigned int le24put(void*, unsigned int);
+unsigned int le32put(void*, unsigned int);
+unsigned int le64put(void*, unsigned long);
 
 #if defined(__cplusplus)
 }
